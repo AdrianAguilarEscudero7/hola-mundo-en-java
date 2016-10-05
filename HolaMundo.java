eda53970ff09f61223c8607a118c638a3dac614c
Adrián Aguilar Escudero <adri.dev7@gmail.com>
2016-10-05 08:57:15 +0200

    Añadido el fichero HolaMundo.java con comentarios

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+/**
+* 
+* Hola Mundo en Java
+*
+*
+* Muestra un hola mundo por pantalla
+*
+* @author Adrián Aguilar
+*/
+public class HolaMundo  {
+  public static void main(String[] args) {  
+  
+      System.out.println("¡Hola Mundo!");
+  
+  }
+}
